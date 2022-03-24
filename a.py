9527d00a163d228747f5306edeef0eaeb956efe8
haruya3 <genga711@gmail.com>
2022-03-24 11:29:09 +0900

    無駄なファイルを削除

--- a.py
+++
@@ -1,8 +0,0 @@
-a = "a"
-data = {
-    f'a={a}'
-}
-print(data)
-
-
-{'access_token': 'eyJhbGciOiJIUzI1NiJ9.miPcsOK8WsgStvJtaDLJrxS0EX7YY5YstwPcxIH9DvyqM9e6FDejm-9u6gsHaf7R9IFoXS7_FyXFWgZHJ-45QsAqVV4Ry148EUt_t5qDMr9AiklxdUoSbosD8n9Rh-sv.zVIAJmTCi4nA3qVwxaj7y7af-sxwk6uuUo4lGm3tMF4', 'token_type': 'Bearer', 'expires_in': 2592000, 'key_id': 'S-NijSmqd2vfTnt6Zv4oaA'}
