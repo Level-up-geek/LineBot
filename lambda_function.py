41bbe747039a1ca6a18ac9f70890d03886e07ed9
haruya3 <genga711@gmail.com>
2022-04-13 15:29:29 +0900

    lambda_function.pyを更新:オウム返しBotを作成

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,54 @@
 import json
-from dotenv import dotenv_values
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+from linebot.exceptions import (
+    LineBotApiError, InvalidSignatureError
+)
+
+from module.esa import Esa
+
+from dotenv import load_dotenv
+load_dotenv()
+import logging, os, sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+channel_secret = os.getenv('LINE_SECRET_KEY', None)
+channel_access_token = os.getenv('ACCESS_TOKEN', None)
+
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    config = dotenv_values(".env")
-    return {
-        'statusCode': 200,
-        'body': json.dumps(config)
-    }
+    if "x-line-signature" in event["headers"]:
+        signature = event["headers"]["x-line-signature"]
+    elif "X-Line-Signature" in event["headers"]:
+        signature = event["headers"]["X-Line-Signature"]
+    body = event["body"]
+    logging.debug(event)
     
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        logger.error("Invalid signature. Please check your channel access token/channel secret.")
+        return {"statusCode": 400}
+    
+    @handler.add(MessageEvent, message=TextMessage)
+    def message(line_event):
+        text = line_event.message.text
+        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text))
+    
+    return {"statusCode": 200, "body": ''}
