7fa7ae13aee01615a5bebd62fbbef4ef7be8c082
haruya3 <genga711@gmail.com>
2022-04-30 15:47:43 +0900

    create-and-store-graph-workflow.ymlの修正 and writing_esaInfo_csv.py: UTC時間ということを考慮した and 実行された日のdate型を取得するようにした

--- writing_esaInfo_csv.py
+++ writing_esaInfo_csv.py
@@ -16,8 +16,7 @@ logger.setLevel(logging.ERROR)
 
 def main(all_get_flag, week_or_month_flag):
     team_name = os.getenv('ESA_TEAM_NAME')
-    #MEMO:todayにしないと、Git Hub Actionsで実行された時の日付が取得できない
-    today = datetime.date(2022, 4, 21)
+    today = datetime.date.today()
     week_number = 0
 
     if week_or_month_flag == 'week':        
